48a07bc875271ce9d4829cb63e35d0fa0183b3b8
Thomas Dupont <thomas.dupont@intec.ugent.be>
2020-12-16 17:14:18 +0100

    Format configuration added

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 local StoredAnimaCounter = LibStub("AceAddon-3.0"):NewAddon("StoredAnimaCounter", "AceBucket-3.0")
+local AceConfig = LibStub("AceConfig-3.0")
+local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 local ldbObject = ldb:NewDataObject("Stored Anima", {
     type = "data source",
@@ -8,8 +10,38 @@ local ldbObject = ldb:NewDataObject("Stored Anima", {
 })
 local bucketListener = nil
 
+local STORED_ONLY = 1
+
+local Format = {
+    stored = 1,
+    stored_plus_pool = 2,
+    pool_plus_stored = 3,
+    sum_only = 4,
+    sum_plus_stored = 5,
+    stored_plus_sum = 6,
+    pool_plus_sum = 7
+
+}
+
+local FormatLabels = {
+    "stored_only",
+    "stored_plus_pool",
+    "pool_plus_stored",
+    "sum_only",
+    "sum_plus_stored",
+    "stored_plus_sum",
+    "pool_plus_sum"
+}
+
+local configIsVerbose = false
+local configFormat = Format.stored
+local addonName = "Stored Anima Counter"
+
+-- Lifecycle functions
+
 function StoredAnimaCounter:OnInitialize()
     print("Addon StoredAnimaCounter Loaded!")
+    StoredAnimaCounter:SetupConfig()
     StoredAnimaCounter:SetupEventListeners()
 end
 
@@ -30,8 +62,66 @@ function StoredAnimaCounter:SetupEventListeners()
     bucketListener = StoredAnimaCounter:RegisterBucketEvent("BAG_UPDATE", 0.2, "ScanForStoredAnima")
 end
 
+-- Config functions
+function StoredAnimaCounter:SetupConfig() 
+    local options =  {
+        name = addonName,
+        handler = StoredAnimaCounter,
+        type = "group",
+        args = {
+          config = {
+              name = "Configuration",
+              desc = "Opens the SAC Configuration panel",
+              type = "execute",
+              func = "OpenConfigPanel"
+          },
+          verbose = {
+            name = "Toggle chat output",
+            desc = "Toggles verbose output in chat",
+            type = "toggle",
+            set = "SetVerbose",
+            get = "GetVerbose"
+          },
+          format = {
+            name = "Choose output format",
+            type = "select",
+            values = FormatLabels,
+            set = "SetFormat",
+            get = "GetFormat"
+          }
+        }
+      }
+      AceConfig:RegisterOptionsTable(addonName, options, {"storedanimacounter", "sac"})
+      configPath = AceConfigDialog:AddToBlizOptions(addonName)
+end
+
+function StoredAnimaCounter:OpenConfigPanel(info)
+    InterfaceOptionsFrame_OpenToCategory(addonName)
+    InterfaceOptionsFrame_OpenToCategory(addonName)
+end
+
+
+function StoredAnimaCounter:SetVerbose(info, toggle)
+    configIsVerbose = toggle
+end
+
+function StoredAnimaCounter:GetVerbose(info)
+    return configIsVerbose
+end
+
+function StoredAnimaCounter:SetFormat(info, toggle)
+    configFormat = toggle
+    StoredAnimaCounter:ScanForStoredAnima(ldbObject.value)
+end
+
+function StoredAnimaCounter:GetFormat(info)
+    return configFormat
+end
+
+-- Anima functions
+
 function StoredAnimaCounter:ScanForStoredAnima()
-    print("Scanning:")
+    vprint("Scanning:")
     local total = 0
     for bag = 0, 4 do
         local slots = GetContainerNumSlots(bag)
@@ -39,9 +129,36 @@ function StoredAnimaCounter:ScanForStoredAnima()
             total = total + (StoredAnimaCounter:doForItemInBag(bag, slot))
         end
     end
-    print(">> Total stored anima: " .. total)
-    ldbObject.value = total
-    ldbObject.text = string.format("%d", total)
+    StoredAnimaCounter:outputValue(total)
+end
+
+function StoredAnimaCounter:outputValue(stored) 
+    vprint(">> Total stored anima: " .. stored)
+    local currencyID = C_CovenantSanctumUI.GetAnimaInfo()
+    local pool = C_CurrencyInfo.GetCurrencyInfo(currencyID).quantity
+    local sum = pool + stored
+    if configFormat == Format.stored then
+        ldbObject.value = stored
+        ldbObject.text = string.format("%d", stored)
+    elseif configFormat == Format.stored_plus_pool then
+        ldbObject.value = stored
+        ldbObject.text = string.format("%d (%d)", stored, pool)
+    elseif configFormat == Format.pool_plus_stored then
+        ldbObject.value = stored
+        ldbObject.text = string.format("%d (%d)", pool, stored)
+    elseif configFormat == Format.sum_only then
+        ldbObject.value = stored
+        ldbObject.text = string.format("%d", sum)
+    elseif configFormat == Format.sum_plus_stored then
+        ldbObject.value = stored
+        ldbObject.text = string.format("%d (%d)", sum, stored)
+    elseif configFormat == Format.stored_plus_sum then
+        ldbObject.value = stored
+        ldbObject.text = string.format("%d (%d)", stored, sum)
+    elseif configFormat == Format.pool_plus_sum then
+        ldbObject.value = stored
+        ldbObject.text = string.format("%d (%d)", pool, sum)
+    end
 end
 
 function StoredAnimaCounter:ttCreate()
@@ -86,13 +203,17 @@ function StoredAnimaCounter:doForItemInBag(bag, slot)
 
         if isAnima then
             totalAnima = (itemCount or 1) * isAnima
-            print("Anima present: " .. totalAnima .. " on " .. itemLink)
+            vprint("Anima present: " .. totalAnima .. " on " .. itemLink)
         end
         tooltip:Hide()        
     end
     return totalAnima
 end
 
+
+function vprint(val)
+   if configIsVerbose then print(val) end
+end
 
 function ldbObject:OnTooltipShow()
     self:AddLine("Stored Anima")
