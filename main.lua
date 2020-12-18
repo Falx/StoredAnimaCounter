13215cf34d7664b2319e21dfaff5f3fb6d690b19
Thomas Dupont <thomas.dupont@intec.ugent.be>
2020-12-18 16:33:26 +0100

    Expanded the tooltip

--- main.lua
+++ main.lua
@@ -1,17 +1,17 @@
-local StoredAnimaCounter = LibStub("AceAddon-3.0"):NewAddon("StoredAnimaCounter", "AceBucket-3.0", "AceEvent-3.0")
+local addonName, addonTable = ...;
+local StoredAnimaCounter = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceBucket-3.0", "AceEvent-3.0")
+
 local AceDB = LibStub("AceDB-3.0")
 local AceConfig = LibStub("AceConfig-3.0")
 local AceConfigDialog = LibStub("AceConfigDialog-3.0")
-local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
-local ldbObject = ldb:NewDataObject("Stored Anima", {
+local LDB = LibStub:GetLibrary("LibDataBroker-1.1")
+
+local ldbObject = LDB:NewDataObject("Stored Anima", {
     type = "data source",
     text = "-",
     value = 0,
     label = "Stored Anima"
 })
-local bucketListener = nil
-
-local STORED_ONLY = 1
 
 local Format = {
     stored = 1,
@@ -27,9 +27,10 @@ local Format = {
 local FormatLabels = {"stored_only", "stored_plus_pool", "pool_plus_stored", "sum_only", "sum_plus_stored",
                       "stored_plus_sum", "pool_plus_sum"}
 
+local tooltip = nil
+local bucketListener = nil
 local configIsVerbose = false
 local configFormat = Format.stored
-local addonName = "Stored Anima Counter"
 
 local defaults = {
     profile = {
@@ -41,17 +42,16 @@ local defaults = {
 -- Lifecycle functions
 
 function StoredAnimaCounter:OnInitialize()
-    print("Addon StoredAnimaCounter Loaded!")
-    StoredAnimaCounter:RegisterEvent("PLAYER_LOGIN", "ScanForStoredAnima")
-    StoredAnimaCounter:SetupEventListeners()
+    print("Addon " .. addonName .. " Loaded!")
     StoredAnimaCounter:SetupDB()
     StoredAnimaCounter:SetupConfig()
-    -- StoredAnimaCounter:RefreshConfig()
 end
 
 function StoredAnimaCounter:OnEnable()
+    -- StoredAnimaCounter:RegisterEvent("PLAYER_LOGIN", "ScanForStoredAnima")
+    self.ScanForStoredAnima()
     if bucketListener == nil then
-        StoredAnimaCounter:SetupEventListeners()
+        bucketListener = StoredAnimaCounter:RegisterBucketEvent("BAG_UPDATE", 0.2, "ScanForStoredAnima")
     end
     StoredAnimaCounter:RefreshConfig()
 end
@@ -63,23 +63,11 @@ function StoredAnimaCounter:OnDisable()
     end
 end
 
-function StoredAnimaCounter:SetupEventListeners()
-    bucketListener = StoredAnimaCounter:RegisterBucketEvent("BAG_UPDATE", 0.2, "ScanForStoredAnima")
-end
-
--- DB functions
-
 function StoredAnimaCounter:SetupDB()
     self.db = AceDB:New("StoredAnimaCounterDB", defaults)
     self.db.RegisterCallback(self, "OnProfileChanged", "RefreshConfig")
     self.db.RegisterCallback(self, "OnProfileCopied", "RefreshConfig")
     self.db.RegisterCallback(self, "OnProfileReset", "RefreshConfig")
-end
-
-function StoredAnimaCounter:RefreshConfig()
-    configIsVerbose = self.db.profile.verbose
-    configFormat = self.db.profile.format
-    StoredAnimaCounter:ScanForStoredAnima()
 end
 
 -- Config functions
@@ -139,8 +127,15 @@ function StoredAnimaCounter:SetupConfig()
         }
     }
     options.args.profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+
     AceConfig:RegisterOptionsTable(addonName, options, {"storedanimacounter", "sac"})
-    configPath = AceConfigDialog:AddToBlizOptions(addonName)
+    AceConfigDialog:AddToBlizOptions(addonName)
+end
+
+function StoredAnimaCounter:RefreshConfig()
+    configIsVerbose = self.db.profile.verbose
+    configFormat = self.db.profile.format
+    StoredAnimaCounter:ScanForStoredAnima()
 end
 
 function StoredAnimaCounter:OpenConfigPanel(info)
@@ -183,8 +178,7 @@ end
 
 function StoredAnimaCounter:outputValue(stored)
     vprint(">> Total stored anima: " .. stored)
-    local currencyID = C_CovenantSanctumUI.GetAnimaInfo()
-    local pool = C_CurrencyInfo.GetCurrencyInfo(currencyID).quantity
+    local pool = GetReservoirAnima()
     local sum = pool + stored
     if configFormat == Format.stored then
         ldbObject.value = stored
@@ -227,11 +221,11 @@ function StoredAnimaCounter:doForItemInBag(bag, slot)
     local itemId = GetContainerItemID(bag, slot)
     local _, itemCount = GetContainerItemInfo(bag, slot)
     local totalAnima = 0
+    local animaCount = 0;
     if itemId ~= nil then
-        local itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,
-            itemEquipLoc, itemTexture, sellPrice, itemClassID, itemSubClassID, bindType, expacID, setID,
-            isCraftingReagent = GetItemInfo(itemId)
-        local tooltip = tooltip or StoredAnimaCounter:ttCreate()
+        local itemLink = select(2, GetItemInfo(itemId))
+        local itemClassID, itemSubClassID = select(12, GetItemInfo(itemId))
+        tooltip = tooltip or StoredAnimaCounter:ttCreate()
         tooltip:SetOwner(UIParent, "ANCHOR_NONE")
         tooltip:ClearLines()
         tooltip:SetBagItem(bag, slot)
@@ -245,12 +239,12 @@ function StoredAnimaCounter:doForItemInBag(bag, slot)
                 isAnima = true
             elseif t and isAnima and t:find("^Use") then
                 local num = t:match("%d+")
-                isAnima = tonumber(num or "")
+                animaCount = tonumber(num or "")
             end
         end
 
-        if isAnima then
-            totalAnima = (itemCount or 1) * isAnima
+        if isAnima and (animaCount > 0) then
+            totalAnima = (itemCount or 1) * animaCount
             vprint("Anima present: " .. totalAnima .. " on " .. itemLink)
         end
         tooltip:Hide()
@@ -264,6 +258,19 @@ function vprint(val)
     end
 end
 
+function GetReservoirAnima()
+    local currencyID = C_CovenantSanctumUI.GetAnimaInfo()
+    return C_CurrencyInfo.GetCurrencyInfo(currencyID).quantity
+end
+
+local NORMAL_FONT_COLOR = {1.0, 0.82, 0.0}
+
 function ldbObject:OnTooltipShow()
-    self:AddLine("Stored Anima")
+    self:AddLine("|cFF2C94FEStored Anima|r")
+    self:AddLine("An overview of anima stored in your bags, but not yet added to your covenant's reservoir.", 1.0, 0.82,
+        0.0, 1)
+    self:AddLine("\n")
+    self:AddDoubleLine("Stored:", "|cFFFFFFFF" .. ldbObject.value .. "|r")
+    self:AddDoubleLine("Reservoir:", "|cFFFFFFFF" .. GetReservoirAnima() .. "|r")
+    self:AddDoubleLine("Total:", "|cFFFFFFFF" .. ldbObject.value + GetReservoirAnima() .. "|r")
 end
